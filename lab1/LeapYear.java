c4fc34300f6022f31ae1b4b321fed3139ae70dc3
mengyingyuan <83455470+mengyingyuan@users.noreply.github.com>
2022-09-16 16:02:59 -0800

    update lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,15 @@
  */
 public class LeapYear {
 
+    public static boolean isLeapYear(int year){
+        if (year%400 == 0){
+            return true;
+        }
+        if ( year %4 == 0 && year %100 != 0){
+            return true;
+        }
+        return false;
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
