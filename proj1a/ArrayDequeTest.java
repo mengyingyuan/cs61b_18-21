7ab27f9700f2fc10ff780d58544db135878e8476
mengyingyuan <83455470+mengyingyuan@users.noreply.github.com>
2022-10-04 14:01:10 -0800

    finished proj1a

---
+++ proj1a/ArrayDequeTest.java
@@ -0,0 +1,119 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+public class ArrayDequeTest {
+    /* Utility method for printing out empty checks. */
+    public static boolean checkEmpty(boolean expected, boolean actual) {
+        if (expected != actual) {
+            System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    /* Utility method for printing out empty checks. */
+    public static boolean checkSize(int expected, int actual) {
+        if (expected != actual) {
+            System.out.println("size() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
+
+    /* Prints a nice message based on whether a test passed.
+     * The \n means newline. */
+    public static void printTestStatus(boolean passed) {
+        if (passed) {
+            System.out.println("Test passed!\n");
+        } else {
+            System.out.println("Test failed!\n");
+        }
+    }
+
+    /** Adds a few things to the list, checking isEmpty() and size() are correct,
+     * finally printing the results.
+     *
+     * && is the "and" operation. */
+    public static void addIsEmptySizeTest() {
+        System.out.println("Running add/isEmpty/Size test.");
+
+        ArrayDeque<String> a1 = new ArrayDeque<String>();
+
+        boolean passed = checkEmpty(true, a1.isEmpty());
+
+        a1.addFirst("front");
+
+        // The && operator is the same as "and" in Python.
+        // It's a binary operator that returns true if both arguments true, and false otherwise.
+        passed = checkSize(1, a1.size()) && passed;
+        passed = checkEmpty(false, a1.isEmpty()) && passed;
+
+        a1.addLast("middle");
+        passed = checkSize(2, a1.size()) && passed;
+
+        a1.addLast("back");
+        passed = checkSize(3, a1.size()) && passed;
+
+        System.out.println("Printing out deque: ");
+        a1.printDeque();
+
+        printTestStatus(passed);
+
+    }
+
+    /** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
+    public static void addRemoveTest() {
+
+        System.out.println("Running add/remove test.");
+
+
+        ArrayDeque<Integer> a1 = new ArrayDeque<Integer>();
+        // should be empty
+        boolean passed = checkEmpty(true, a1.isEmpty());
+
+        a1.addFirst(10);
+        // should not be empty
+        passed = checkEmpty(false, a1.isEmpty()) && passed;
+
+        a1.removeFirst();
+        // should be empty
+        passed = checkEmpty(true, a1.isEmpty()) && passed;
+
+        printTestStatus(passed);
+
+    }
+
+    //check what happens if the data structure goes from empty, to some non-zero size (e.g. 4 items)
+    // back down to zero again, and then back to some non-zero size. This is a common oversight.
+    public static void printTest() {
+        ArrayDeque<Integer> a1 = new ArrayDeque<Integer>();
+        for(int i =0; i< 4; i+=1) {
+            a1.addFirst(1);
+            a1.addFirst(2);
+            a1.addFirst(3);
+            a1.addLast(4);
+            a1.printDeque();
+            System.out.println(", size = " + a1.size());
+        }
+
+        for(int i =0; i< 4; i+=1) {
+            a1.removeFirst();
+            a1.removeLast();
+            a1.removeFirst();
+            a1.removeLast();
+        }
+        System.out.print(a1.isEmpty()); // True
+        a1.addLast(3);
+        a1.addFirst(1);
+        a1.addLast(4);
+        a1.printDeque(); // 1 3 4
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running tests.\n");
+        addIsEmptySizeTest();
+        addRemoveTest();
+        printTest();
+    }
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,105 @@
+public class LinkedListDeque<T> {
+    public Node sentinel;
+    public int size;
+
+    public class Node{
+        private Node prev;
+        private T item;
+        private Node next;
+
+        public Node(){
+            prev = null;
+            next = null;
+        }
+
+        public Node(Node pre, T itm, Node n){
+            prev =pre;
+            item = itm;
+            next = n;
+        }
+    }
+
+    // Creates an empty linked list deque.
+    public LinkedListDeque(){
+        sentinel = new Node();
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
+
+    public void addFirst(T item){
+        Node p = new Node(sentinel, item, sentinel.next);
+        sentinel.next.prev = p;
+        sentinel.next = p;
+        size += 1;
+
+    }
+
+    public void addLast(T item){
+        Node p = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.next = p;
+        sentinel.prev = p;
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    //Prints the items in the deque from first to last, separated by a space
+    public void printDeque(){
+        Node p = sentinel.next;
+        while(p != sentinel){
+            System.out.print(p.item + " ");
+            p = p.next;
+        }
+    }
+
+    // Removes and returns the item at the front of the deque. If no such item exists, returns null.
+    public T removeFirst(){
+        if(sentinel.next == sentinel){
+            return null;
+        }
+        T first = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size -= 1;
+        return first;
+    }
+
+    public T removeLast(){
+        if(sentinel.prev == sentinel){
+            return null;
+        }
+        T last = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size -= 1;
+        return last;
+    }
+
+    public T get(int index){
+        Node p = sentinel.next;
+        for(int i=0; i<index; i+=1) {
+            p = p.next;
+        }
+        return p.item;
+    }
+
+    public T getRecursive(int index){
+        Node p = sentinel.next;
+        return getRecursiveHelper(p, index);
+    }
+
+    public T getRecursiveHelper(Node p, int index){
+        if (index == 0) {
+            return p.item;
+        }
+        p = p.next;
+        return getRecursiveHelper(p, index-1);
+    }
+}
